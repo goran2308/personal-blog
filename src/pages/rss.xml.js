425ba911890071797ab216b235e57b70dff5d710
Goran <solev.goran@gmail.com>
2023-02-06 14:06:32 +0000

    Removed About page as not needed. Added styyles to the RSS feed.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 export async function get() {
   return rss({
     title: "Goran Solev | Blog",
-    description: "Personal blog",
+    description: "Learn the web, the right way!",
     site: "https://goransolev1.netlify.app/",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-us</language>`,
