6d4087d0281b2476b358ed337cd25d5b68a35c48
Goran <solev.goran@gmail.com>
2023-02-06 20:25:15 +0000

    RSS Bugs fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,6 @@ export async function get() {
     description: "Learn the web, the right way!",
     site: "https://goransolev1.netlify.app/",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-    stylesheet: "/public/rss/rss-styles.xsl",
     customData: `<language>en-us</language>`,
   })
 }
