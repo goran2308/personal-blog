c7b8d64c080c505bd9fe0b9a6d45cfa21bd91d39
Goran <solev.goran@gmail.com>
2023-02-06 20:07:12 +0000

    RSS Bugs fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-export default defineConfig(
-  {
-    integrations: [tailwind()],
-  },
-  {
-    site: "https://goransolev1.netlify.app/",
-  }
-)
+export default defineConfig({
+  integrations: [tailwind()],
+})
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get(context) {
   return rss({
     title: "Goran Solev | Blog",
     description: "Learn the web, the right way!",
-    site: context.site,
+    site: "https://goransolev1.netlify.app/",
     items: await pagesGlobToRssItems(import.meta.glob("./blog/*.{md,mdx}")),
     stylesheet: "rss/rss-styles.xsl",
   })
