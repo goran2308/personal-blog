5520e81a9a9ec278c2f5abe847eb212ae55df370
Goran <solev.goran@gmail.com>
2023-02-06 19:59:14 +0000

    RSS Bugs fixe

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 
-export async function get() {
+export async function get(context) {
   return rss({
     title: "Goran Solev | Blog",
     description: "Learn the web, the right way!",
-    site: "https://goransolev1.netlify.app/",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-    customData: `<language>en-us</language>`,
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.{md,mdx}")),
     stylesheet: "rss/rss-styles.xsl",
   })
 }
