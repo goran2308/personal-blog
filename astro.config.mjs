9824b96268fa70a3d8fc0b15239bfa2bf2175f0c
Goran <solev.goran@gmail.com>
2025-12-11 04:31:29 +0000

    Installed gtag all over again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown"
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +13,9 @@ export default defineConfig({
     partytown({
       config: {
         forward: ["dataLayer.push"],
+        debug: true,
       },
     }),
   ],
   site: "https://goransolev.com",
-})
+});
