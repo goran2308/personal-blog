4cdc934e2a65c63ecf2800ae5b50b6df482d5fd5
Goran <solev.goran@gmail.com>
2025-12-18 06:18:55 +0000

    Added the Umami website id

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-import yeskunallumami from "@yeskunall/astro-umami";
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), yeskunallumami()],
+  integrations: [
+    tailwind(),
+    umami({
+      id: "97495836-129a-430e-af03-6d2ef6cb3b94",
+    }),
+  ],
   site: "https://goransolev.com",
 });
