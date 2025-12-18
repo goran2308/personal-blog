b18e72c5a3efbc2d3c7b28650e87c16426f7aae0
Goran <solev.goran@gmail.com>
2025-12-18 06:06:45 +0000

    Removed partytown, installed Umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
+import yeskunallumami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-        debug: true,
-      },
-    }),
-  ],
+  integrations: [tailwind(), yeskunallumami()],
   site: "https://goransolev.com",
 });
