61ace4b0008fa97e330ed433d056d95ea923f3ae
Goran <solev.goran@gmail.com>
2026-02-26 03:47:40 +0000

    Refactor index page styles and update Tailwind configuration with custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "goran-dark": "#0E273C",
+        "goran-dark-hover": "#0A1C2E",
+        "goran-dark-light": "#1A3B55",
+        "goran-dark-muted": "#334455",
+        "goran-dark-muted-darker": "#1F2A3A",
+        "goran-dark-muted-darkest": "#17212E",
+        "goran-accent": "#0EA5E9",
+        "goran-success": "#15803D",
+      },
+    },
+  },
+  plugins: [],
+};
